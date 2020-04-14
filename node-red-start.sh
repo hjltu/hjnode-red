3a8118f919e2e7bdfd1b55828bfac733ed5a3d5a
hjltu <hjltu@ya.ru>
2020-04-14 12:55:17 +0100

    added BAOS and iPort IPs

--- node-red-start.sh
+++ node-red-start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-MQTT_LOCAL="localhost"
-MQTT_REMOTE=""
 CONFIG_FILE="hjhome.py"
+MQTT_DEFAULT="localhost"
+BAOS_DEFAULT="192.168.0.40"
+IPORT_DEFAULT="192.168.0.20"
+MQTT_REMOTE=""
+BAOS_IP=""
+IPORT_IP=""
 
 while read name and value
 do
@@ -10,14 +14,28 @@ do
         echo "MQTT Server IP Addr = $value"
         MQTT_REMOTE=$value
     fi
+    if [ $name = 'BAOS_IP' ]; then
+        echo "BAOS IP Addr = $value"
+        BAOS_IP=$value
+    fi
+    if [ $name = 'IPORT_IP' ]; then
+        echo "iPort IP Addr = $value"
+        IPORT_IP=$value
+    fi
 done < /root/config/$CONFIG_FILE
 
 echo "cp flow file"
 HOST=`hostname`
 cp /root/config/flows_hjnode-red.json /root/.node-red/flows_$HOST.json
 echo "change MQTT server IP"
-sed -i "s/$MQTT_LOCAL/$MQTT_REMOTE/g" /root/.node-red/flows_$HOST.json
+sed -i "s/$MQTT_DEFAULT/$MQTT_REMOTE/g" /root/.node-red/flows_$HOST.json
 sed -i "s/\"$MQTT_REMOTE\"/$MQTT_REMOTE/g" /root/.node-red/flows_$HOST.json
+echo "change BAOS IP"
+sed -i "s/$BAOS_DEFAULT/$BAOS_IP/g" /root/.node-red/flows_$HOST.json
+sed -i "s/\"$BAOS_IP\"/$BAOS_IP/g" /root/.node-red/flows_$HOST.json
+echo "change iPort IP"
+sed -i "s/$IPORT_DEFAULT/$IPORT_IP/g" /root/.node-red/flows_$HOST.json
+sed -i "s/\"$IPORT_IP\"/$IPORT_IP/g" /root/.node-red/flows_$HOST.json
 
 echo "start node-red"
 node-red
